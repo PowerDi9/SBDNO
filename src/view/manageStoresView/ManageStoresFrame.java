cecccfaa4909bf2a2c9dafedb467d003acfaf49f
AGH <adriangonzalezhermida68@gmail.com>
2025-12-04 12:45:56 +0100

    License added

--- src/view/manageStoresView/ManageStoresFrame.java
+++ src/view/manageStoresView/ManageStoresFrame.java
@@ -1,6 +1,20 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ * SBDNO - Small Business Delivery Note Organizer
+ * 
+ * Copyright (C) 2025 Adrián González Hermida
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package view.manageStoresView;
 
